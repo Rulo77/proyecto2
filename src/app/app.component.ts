494f8192fa2fd75c971de43f55d96eed7ae53363
Rulo77 <ragomez1234567@gmail.com>
2024-08-15 09:49:54 -0600

    prueba para deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practicaFront';
+  contador:number = 0;
+
+  sumContador(){
+    this.contador++;
+  }
 }
